76e9e270e1d4de1a0f284931b7e29d9afb7f1519
andersdd <dave@pentuple.co.nz>
2015-11-04 10:31:59 +1300

    Add 12 Months Sales History to Inventory Days report (selectable by user)

--- inventorydays/client/scripts/dspInventoryDays.js
+++ inventorydays/client/scripts/dspInventoryDays.js
@@ -1,38 +1,25 @@
 include("inventoryDays");
 
-var _month1;
-var _month2;
-var _month3;
+var _month = new Array();
 var _hdr = "Sales ";
 
 mywindow.setWindowTitle(qsTr("Inventory Days of Sales"));
 mywindow.setListLabel(qsTr("Inventory Days"));
+mywindow.setMetaSQLOptions('inventoryDays','detail');
+mywindow.setQueryOnStartEnabled(false);
+mywindow.setSearchVisible(false);
+mywindow.setReportName("InventoryDays");
 
 //mywindow.setUseAltId(true);
 
-// Specifiy which query to use
-mywindow.setMetaSQLOptions('inventoryDays','detail');
- 
-// Set automatic query on start
-mywindow.setQueryOnStartEnabled(false);
- 
-// Make the search visible/ not visible
-mywindow.setSearchVisible(false);
-
-// Set the Report
-mywindow.setReportName("InventoryDays");
-
 // Get Month Headings
-var _sql = "SELECT to_char((current_date - INTERVAL '1 mon')::date, 'Mon-YYYY') as month1, "
-	+ " to_char((current_date - INTERVAL '2 mon')::date, 'Mon-YYYY') as month2, "
-	+ " to_char((current_date - INTERVAL '3 mon')::date, 'Mon-YYYY') as month3 "
-var data = toolbox.executeQuery(_sql, new Object());
-if (data.first())
- {
-   _month1 = _hdr + data.value("month1");
-   _month2 = _hdr + data.value("month2");
-   _month3 = _hdr + data.value("month3");
- }
+for (var i = 1; i < 13; i++)
+{
+  var _sql = "SELECT to_char((current_date - INTERVAL '"+ i +" mon')::date, 'Mon-YYYY') as month;";
+  var data = toolbox.executeQuery(_sql, {});
+  if (data.first())
+    _month[i] = _hdr + data.value("month");
+}
 
 // Add in the columns
 var _list = mywindow.list();
@@ -42,9 +29,9 @@ if (metrics.value("MultiWhs") == 't')
 _list.addColumn(qsTr("Class Code"), XTreeWidget.itemColumn, Qt.AlignLeft, true, "classcode_code");
 _list.addColumn(qsTr("Item Number"), XTreeWidget.itemColumn, Qt.AlignLeft, true, "item_number");
 _list.addColumn(qsTr("Item Description"), -1, Qt.AlignLeft, true, "item_descrip1");
-_list.addColumn(_month3, XTreeWidget.qtyColumn, Qt.AlignRight, true, "month3_qty");
-_list.addColumn(_month2, XTreeWidget.qtyColumn, Qt.AlignRight, true, "month2_qty");
-_list.addColumn(_month1, XTreeWidget.qtyColumn, Qt.AlignRight, true, "month1_qty");
+_list.addColumn(_month[3], XTreeWidget.qtyColumn, Qt.AlignRight, true, "month3_qty");
+_list.addColumn(_month[2], XTreeWidget.qtyColumn, Qt.AlignRight, true, "month2_qty");
+_list.addColumn(_month[1], XTreeWidget.qtyColumn, Qt.AlignRight, true, "month1_qty");
 _list.addColumn(_hdr + qsTr("MTD"), XTreeWidget.qtyColumn, Qt.AlignRight, true, "current_month_qty");
 _list.addColumn(qsTr("QOH"), XTreeWidget.qtyColumn, Qt.AlignRight, true, "qoh");
 _list.addColumn(qsTr("On Order"), XTreeWidget.qtyColumn, Qt.AlignRight, true, "qtyonorder");
@@ -61,3 +48,37 @@ mywindow.parameterWidget().appendComboBox(qsTr("Class Code"), "classcode", sql,n
 mywindow.parameterWidget().append(qsTr("Item"), "item_id",   ParameterWidget.Item);
 mywindow.parameterWidget().append(qsTr("Cutoff Date"), "cutoff",   ParameterWidget.Date);
 mywindow.parameterWidget().append(qsTr("Show Obsolete"), "showObsolete",   ParameterWidget.Exists);
+mywindow.parameterWidget().append(qsTr("Show 12 Months History"), "show12months",   ParameterWidget.Exists);
+
+function inventoryDaysQuery()
+{
+// If "Show 12 Months" is selected we have to add list columns
+  var params = mywindow.parameterWidget().parameters();
+  _list.setColumnCount(0);
+  _colCount = (params.show12months) ? 12 : 3;
+  
+  with (_list)
+  {
+    if (metrics.value("MultiWhs") == 't')
+      addColumn(qsTr("Site"), XTreeWidget.itemColumn, Qt.AlignLeft, true, "warehous_code");
+    addColumn(qsTr("Class Code"), XTreeWidget.itemColumn, Qt.AlignLeft, true, "classcode_code");
+    addColumn(qsTr("Item Number"), XTreeWidget.itemColumn, Qt.AlignLeft, true, "item_number");
+    addColumn(qsTr("Item Description"), -1, Qt.AlignLeft, true, "item_descrip1");
+    for (var i = _colCount; i > 0; i--)
+    {
+      var _col = "month" + i + "_qty";
+      addColumn(_month[i], XTreeWidget.qtyColumn, Qt.AlignRight, true, _col);
+    }
+    addColumn(_hdr + qsTr("MTD"), XTreeWidget.qtyColumn, Qt.AlignRight, true, "current_month_qty");
+    addColumn(qsTr("QOH"), XTreeWidget.qtyColumn, Qt.AlignRight, true, "qoh");
+    addColumn(qsTr("On Order"), XTreeWidget.qtyColumn, Qt.AlignRight, true, "qtyonorder");
+    if (privileges.check("ViewInventoryValue"))
+      addColumn(qsTr("Inventory Value"), XTreeWidget.moneyColumn, Qt.AlignRight, true, "inventory_value");
+    addColumn(qsTr("Inventory Days"), -1, Qt.AlignRight, true, "inventorydays");  
+  }
+  mywindow.sFillList();
+}
+
+toolbox.coreDisconnect(mywindow.queryAction(), "triggered()", mywindow, "sFillList()");
+mywindow.queryAction().triggered.connect(inventoryDaysQuery);
+
